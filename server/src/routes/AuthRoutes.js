328b30eba3c210af20187d16f4bd53ac21deab30
ibrahim0891 <nijhum0891@gmail.com>
2024-11-09 09:00:22 +0600

    removed server from this repo

--- server/src/routes/AuthRoutes.js
+++
@@ -1,9 +0,0 @@
-const { Register, Login, Logout } = require("../controller/AuthController");
-
-const AuthRoute = require("express").Router();
-
-AuthRoute.post("/signup", Register);
-AuthRoute.post("/login", Login);
-AuthRoute.post("/logout", Logout);
-
-module.exports = AuthRoute;
