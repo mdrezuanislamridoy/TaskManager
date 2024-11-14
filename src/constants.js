5a9561443059e23f6c19d6cee28aa2322d4f7185
ibrahim0891 <nijhum0891@gmail.com>
2024-11-14 18:58:38 +0600

    Created dev branch for local testing

--- src/constants.js
+++ src/constants.js
@@ -3,6 +3,6 @@
 
 
 const constants = {
-    serverUrl: typeof process !== 'undefined' && process.env ? process.env.SERVER_URL : "https://taskmanager-server-production.up.railway.app",
+    serverUrl: process.env.SERVER_URL 
 };
 export default constants;
