635e7f99557d52e5a82f2bf47c9bc86d11e35def
ibrahim0891 <nijhum0891@gmail.com>
2024-11-14 19:31:32 +0600

    j

--- src/constants.js
+++ src/constants.js
@@ -3,6 +3,6 @@
 
 
 const constants = {
-    serverUrl: typeof process !== 'undefined' && process.env ? process.env.SERVER_URL : "https://taskmanager-server-production.up.railway.app",
+    serverUrl: 'http://localhost:3003',
 };
 export default constants;
